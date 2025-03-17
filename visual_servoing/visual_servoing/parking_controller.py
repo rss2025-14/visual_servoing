bd629faa7307f37dcde7ab7bf915c83eff1d1e80
Jayson <jlin9811@gmail.com>
2025-03-16 20:28:43 -0400

    Implemented Pure Pursuit for simulation

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -16,7 +16,7 @@ class ParkingController(Node):
     def __init__(self):
         super().__init__("parking_controller")
 
-        self.declare_parameter("drive_topic")
+        self.declare_parameter("drive_topic", "/drive")
         DRIVE_TOPIC = self.get_parameter("drive_topic").value # set in launch file; different for simulator vs racecar
 
         self.drive_pub = self.create_publisher(AckermannDriveStamped, DRIVE_TOPIC, 10)
@@ -29,7 +29,29 @@ class ParkingController(Node):
         self.relative_x = 0
         self.relative_y = 0
 
+        # Used for Pure Pursuit
+        self.look_ahead = 0.5 # Lookahead distance
+        self.wheelbase = 0.34 # Length of wheelbase
+
+        # Used for realignment
+        self.mb = False # Boolean used to reverse when robot is close to cone but not facing it
+        self.max_steer_angle = 0.34
+        self.backward_count = 0
+
         self.get_logger().info("Parking Controller Initialized")
+
+    def find_ref_point(self, cx, cy):
+        """
+        Compute a reference point at `look_ahead` distance in the direction of the cone.
+        If the cone is closer than that, the reference point is the cone itself.
+        """
+        la_dist = self.look_ahead
+        cone_pos = np.array([cx, cy])
+        dist = np.sqrt(cx**2 + cy**2)
+        if dist <= la_dist:
+            return cone_pos, dist
+        else:
+            return ((cone_pos / dist) * la_dist, dist)
 
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
@@ -40,6 +62,36 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Use relative position and your control law to set drive_cmd
+
+        L = self.wheelbase
+        L1 = self.look_ahead
+
+        # Find the next reference point to pursue and calculate distance error and angle of reference point
+        ref, c_dist = self.find_ref_point(self.relative_x, self.relative_y) 
+        rx, ry = ref
+        dist_err = c_dist - self.parking_distance
+        eta = np.arctan2(ry, rx) 
+
+        # If the robot is outside of 0.01 meters from the cone, move towards the cone with pure pursuit
+        # Else if the robot is within 0.01 meters from the cone, stop and align with the cone
+        if self.mb is False:
+            if dist_err > 0.01:
+                speed= 1.0
+                steer_angle = np.arctan2(2*np.sin(eta)*L, L1)  
+            else:
+                cone_angle = np.arctan2(self.relative_y, self.relative_x)
+                if np.abs(cone_angle) > np.radians(10): 
+                    self.mb = True
+                speed= 0.0
+                steer_angle = 0.0
+        else:
+            pass #TODO Implement realignment
+
+
+        drive_cmd.header.stamp = self.get_clock().now().to_msg()
+        drive_cmd.header.frame_id = "base_link"
+        drive_cmd.drive.steering_angle = steer_angle
+        drive_cmd.drive.speed = speed
 
         #################################
 
@@ -57,6 +109,11 @@ class ParkingController(Node):
 
         # YOUR CODE HERE
         # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
+
+        x, y = self.relative_x, self.relative_y
+        error_msg.x_error = x
+        error_msg.y_error = y
+        error_msg.distance_error = np.sqrt(x**2 + y**2)
 
         #################################
         
